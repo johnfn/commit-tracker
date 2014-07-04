3bb5aea6313d77102f5ab7b91a2036bd0c5fb0f9
johnfn <johnfn@gmail.com>
2014-07-04 02:48:39 -0400

    Screw Aeson, I'm making my own JSON parser.

--- Main.hs
+++ Main.hs
@@ -1,12 +1,67 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 
+import GHC.Generics
 import Network.HTTP.Conduit
 import Control.Monad
 import System.Environment
-import Data.List
 import Data.Maybe
+import Data.List.Split
+import Data.List
+import Debug.Trace
+
 import qualified Data.ByteString.Lazy as L
 import Control.Monad.IO.Class (liftIO)
+
+data JSON = JVal String | JObj [(String, JSON)] | JArr [JSON] deriving Show
+
+data Tree = Node [String] [Tree] | Leaf deriving Show
+
+getDepths :: [String] -> [(String, Int)]
+getDepths list =
+    reverse $ foldl go [("", 0)] list
+  where
+    go wholething@((result, depth):rest) "{" = [("", depth + 1)] ++ wholething
+    go wholething@((result, depth):rest) "}" = [("", depth - 1)] ++ wholething
+    go ((result, depth):rest) chunk          = [(result ++ chunk, depth)] ++ rest
+
+
+toTree :: [(String, Int)] -> Tree
+toTree [] = Leaf
+toTree list =
+    Node onLevel $ map toTree (map reduceDepths notOnLevel)
+  where
+    reduceDepths l = map (\(result, depth) -> (result, depth - 1)) l
+    isThisLevel (str, depth) = depth == 0
+    onLevel :: [String] = map fst $ filter isThisLevel list
+    notOnLevel = splitWhen isThisLevel list
+
+{-
+toNestedList :: String -> Tree
+toNestedList str =
+    if length parts == 1 then
+      Value (head parts)
+    else
+
+  where
+    parts :: [String] = split (oneOf "{}") str
+    depths :: [(String, Int)] =
+-}
+
+parseJSON :: String -> Tree
+parseJSON str = toTree $ getDepths $ split (oneOf "{}") str
+
+
+-- parseJSON :: String -> JSON
+
+{-
+instance FromJSON Commits where
+  parseJSON (Object o) = do
+    commits <- mapM parseJSON . filter (\(Object ref) -> True) =<< o .: "references"
+    commits
+-}
 
 userName :: IO String
 userName = do
